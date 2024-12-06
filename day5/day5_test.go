642505741f8df9ce5de80836ca21bfd0be725419
Alim Nastaev <32502172+alimnastaev@users.noreply.github.com>
2024-12-05 19:46:13 -0500

    day5: part 2

--- day5/day5_test.go
+++ day5/day5_test.go
@@ -3,6 +3,7 @@ package day5_test
 // https://adventofcode.com/2024/day/5
 
 import (
+	"sort"
 	"strings"
 	"testing"
 
@@ -19,8 +20,8 @@ func Test_day2(t *testing.T) {
 	}{
 		{"Day 5: Part 1 - Example", "example.txt", 143, day5Part1},
 		{"Day 5: Part 1 - Input", "input.txt", 5639, day5Part1},
-		// {"Day 5: Part 2 - Example", "example.txt", 123, day5Part2},
-		// {"Day 5: Part 2 - Input", "input.txt", 692, day5Part2},
+		{"Day 5: Part 2 - Example", "example.txt", 123, day5Part2},
+		{"Day 5: Part 2 - Input", "input.txt", 5273, day5Part2},
 	}
 
 	for _, tt := range tests {
@@ -31,8 +32,72 @@ func Test_day2(t *testing.T) {
 }
 
 func day5Part1(path string) int {
-	var rules [][]int
-	var updates [][]int
+	return calculateMiddleSum(func() ([][]int, [][]int) { return parseInput(path) }, didFollowRules, false)
+}
+
+func day5Part2(path string) int {
+	return calculateMiddleSum(func() ([][]int, [][]int) { return parseInput(path) }, didFollowRules, true)
+}
+func didFollowRules(update []int, rules [][]int) bool {
+	numToIndexMap := make(map[int]int, len(update))
+	for i, num := range update {
+		numToIndexMap[num] = i
+	}
+
+	for _, r := range rules {
+		idx1, ok1 := numToIndexMap[r[0]]
+		idx2, ok2 := numToIndexMap[r[1]]
+
+		if ok1 && ok2 && idx1 >= idx2 {
+			return false
+		}
+	}
+	return true
+}
+
+func calculateMiddleSum(
+	inputData func() ([][]int, [][]int),
+	validate func([]int, [][]int) bool,
+	reorder bool,
+) int {
+	rules, updates := inputData()
+	sum := 0
+	for _, update := range updates {
+		if validate(update, rules) {
+			if !reorder {
+				sum += update[len(update)/2]
+			}
+		} else if reorder {
+			corrected := reorderUpdate(update, rules)
+			sum += corrected[len(corrected)/2]
+		}
+	}
+	return sum
+}
+
+func reorderUpdate(update []int, rules [][]int) []int {
+	ruleMap := make(map[int][]int)
+	for _, r := range rules {
+		ruleMap[r[0]] = append(ruleMap[r[0]], r[1])
+	}
+
+	sorted := append([]int(nil), update...)
+	sort.SliceStable(sorted, func(i, j int) bool {
+		a, b := sorted[i], sorted[j]
+		for _, dependent := range ruleMap[a] {
+			if dependent == b {
+				return true
+			}
+		}
+
+		return false
+	})
+
+	return sorted
+}
+
+func parseInput(path string) ([][]int, [][]int) {
+	var rules, updates [][]int
 	emptyLineFound := false
 
 	for _, line := range utils.ReadFile(path) {
@@ -42,51 +107,21 @@ func day5Part1(path string) int {
 		}
 
 		if emptyLineFound {
-			updates = append(updates, parseOrders(line)...)
+			updates = append(updates, parseOrders(line))
 		} else {
-			rules = append(rules, parseRules(line)...)
+			rules = append(rules, parseRules(line))
 		}
 	}
 
-	result := 0
-	for _, update := range updates {
-		if didFollowRules(update, rules) {
-			result += update[len(update)/2]
-		}
-	}
-
-	return result
+	return rules, updates
 }
 
-func didFollowRules(update []int, rules [][]int) bool {
-	updatesMap := make(map[int]int, len(update))
-	for i, n := range update {
-		updatesMap[n] = i
-	}
-
-	for _, r := range rules {
-		index1, found1 := updatesMap[r[0]]
-		index2, found2 := updatesMap[r[1]]
-
-		if found1 && found2 && index1 >= index2 {
-			return false
-		}
-	}
-
-	return true
-}
-func parseRules(line string) [][]int {
+func parseRules(line string) []int {
 	parts := strings.Split(line, "|")
-
-	rule := []int{
-		utils.ParseInt(parts[0]),
-		utils.ParseInt(parts[1]),
-	}
-
-	return [][]int{rule}
+	return []int{utils.ParseInt(parts[0]), utils.ParseInt(parts[1])}
 }
 
-func parseOrders(line string) [][]int {
+func parseOrders(line string) []int {
 	parts := strings.Split(line, ",")
 	order := make([]int, len(parts))
 
@@ -94,12 +129,5 @@ func parseOrders(line string) [][]int {
 		order[i] = utils.ParseInt(part)
 	}
 
-	return [][]int{order}
-}
-
-func day5Part2(path string) int {
-	file := utils.ReadFile(path)
-	_ = file
-
-	return 0
+	return order
 }
